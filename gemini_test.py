8eb98e2c7e5072e6b1354151b76f41c1f0820c93
aasthak15 <aastha.khinvasara23@gmail.com>
2026-09-03 01:18:59 +0530

    Update gemini_test.py

--- gemini_test.py
+++ gemini_test.py
@@ -1,6 +1,12 @@
+import os
 from google import genai
+from dotenv import load_dotenv
 
-client = genai.Client(api_key="AQ.Ab8RN6I4PnLoF1Kw3XB5pv_Qb4czOjB_WXAE8XzXosG1St_9Uw")
+load_dotenv()
+
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
 
 response = client.models.generate_content(
     model="gemini-3.6-flash",
